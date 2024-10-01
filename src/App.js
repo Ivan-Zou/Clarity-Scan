88c4121c3384c24cf67c7f7f0344385bbb05f92c
Ivan Zou <ivanzou@seas.upenn.edu>
2024-10-01 19:50:04 -0400

    Set up skeleton code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() {
+    const [currentTab, setCurrentTab] = useState('addPurchase');
+    
+    return (
+        <div className="App">
+            <div>
+                Hello World!
+            </div>
+            
+        </div>
+    );
+}
+
+export default App;
