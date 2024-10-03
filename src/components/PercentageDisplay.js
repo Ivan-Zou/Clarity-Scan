7477c8a24f04fac92d5a60c12316ca4baf47571a
Ivan Zou <ivanzou@seas.upenn.edu>
2024-10-03 16:04:47 -0400

    created percentage display component

---
+++ src/components/PercentageDisplay.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const PercentageDisplay = ({percent}) => {
+    var color = percent < 50 ? '#00ff00' : '#ff0000'
+    return (
+        <div className="circular-progress-bar-container">
+            <CircularProgressbar
+                value={percent}
+                text={`${percent}%`}
+                styles={buildStyles({
+                    trailColor: '#d2d2d2',
+                    pathColor: color,
+                    textColor: color,
+                    textSize: '36px',
+                })}
+            />
+        </div>
+    );
+};
+
+export default PercentageDisplay;
