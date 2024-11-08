ce4636f10bc8823bd93d64b79611d683c8fc8eab
bradyz314 <bradyzhou314159@gmail.com>
2024-11-07 21:30:56 -0500

    Fix transcript extraction to work for consecutive videos

--- build/content_script.js
+++ build/content_script.js
@@ -1,19 +1,20 @@
 const VIDEO_PATHNAME = '/watch';
+let detectedMutation = false;
 
-function openTranscript(_mutations, observer) {
+// Simulate clicking the "View Transcript" button on a YouTube video. Return True on success, False otherwise.
+function openTranscript() {
     const viewTranscriptBtn = document.querySelector('[aria-label="Show transcript"]');
     if (viewTranscriptBtn) {
         viewTranscriptBtn.click();
-        if (observer) {
-            observer.disconnect();
-        }
+        return true;
     }
+    return false;
 }
 
-function openPopUp(mutations = null, observer = null) {
+// Opens the main pop up window.
+function openPopUp(_mutations = null, observer = null) {
     const url = new URL(window.location.href)
     if (url.pathname === VIDEO_PATHNAME) {
-        openTranscript(mutations, observer)
         chrome.runtime.sendMessage({ action: 'open_popup' });
         if (observer) {
             observer.disconnect();
@@ -21,6 +22,7 @@ function openPopUp(mutations = null, observer = null) {
     }
 }
 
+// Get the transcript of a YouTube video.
 function getTranscript(_mutations = null, observer = null) {
     const url = new URL(window.location.href)
     if (url.pathname === VIDEO_PATHNAME) {
@@ -39,6 +41,7 @@ function getTranscript(_mutations = null, observer = null) {
     }
 }
 
+// Simulate clicking the "Close Transcript" button on a YouTube video.
 function closeTranscript() {
     const closeTranscriptBtn = document.querySelector('[aria-label="Close transcript"]');
     if (closeTranscriptBtn) {
@@ -46,16 +49,37 @@ function closeTranscript() {
     }
 }
 
+// Adds a MutationObserver that calls the speicifed callback.
 function addObserver(callback, subtree = false) {
     const observer = new MutationObserver(callback);
     observer.observe(document.body,
         { attributes: false, childList: true, subtree: subtree });
 }
 
+// Add MutationObservers to open the popup and get the transcript. Open the transcript after a short delay to trigger the callbacks.
+function addObservers() {
+    addObserver(openPopUp);
+    addObserver(getTranscript, true);
+    setTimeout(() => {
+        openTranscript();
+    }, 250);
+}
+
+// Callback that sets the detectedMutation flag to false after 1 second of idle DOM.
+function checkMutations() {
+    detectedMutation = true;
+    setTimeout(() => {
+        detectedMutation = false;
+    }, 1000);
+}
+
 window.addEventListener('yt-navigate-finish', () => {
     console.log('navigate finish');
-    setTimeout(() => {
-        addObserver(openPopUp);
-        addObserver(getTranscript, true);
+    addObserver(checkMutations);
+    let intervalId = setInterval(() => {
+        if (!detectedMutation) {
+            addObservers();
+            clearInterval(intervalId);
+        }
     }, 1500);
 });
