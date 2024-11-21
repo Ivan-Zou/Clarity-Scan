e72cf4a78aa9ebd60295c223a3064f6f9defae88
Ivan Zou <ivanzou@seas.upenn.edu>
2024-11-21 10:09:07 -0500

    created way to generate random percentage using python code and connect to javascript chrome extension

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PercentageDisplay from './components/PercentageDisplay';
 import './App.css';
 
 
 function App() {
-    const [percent, setPercentage] = useState(65);
+    const [percent, setPercentage] = useState(0);
+
+    useEffect(() => {
+        // Connect to the WebSocket server
+        const socket = new WebSocket('ws://localhost:6789');
+
+        // Update the percentage when a message is received
+        socket.onmessage = (event) => {
+            const percentage = parseInt(event.data, 10);
+            setPercentage(percentage);
+        };
+
+        // Clean up the WebSocket connection on unmount
+        return () => {
+            socket.close();
+        };
+    }, []);
+
     return (
         <div className="App">
             <div>
